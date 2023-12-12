042cc1fc66600f37908315b152dae4dc4b42056f
yunan <yunanchang@yahoo.com.tw>
2023-12-12 23:10:12 +0800

    d

--- api/edit.php
+++ api/edit.php
@@ -10,29 +10,32 @@ if(isset($_POST['id'])){
     }
 }
  */
+foreach($_POST['id'] as  $key => $id){
+    if(isset($_POST['del']) && in_array($id,$_POST['del'])){
+        $DB->del($id);
+    }else{
+        $row=$DB->find($id);
+        if(isset($row['text'])){
+            $row['text']=$_POST['text'][$key];
+        }
+        switch($table){
+            case "title":
+                $row['sh']=(isset($_POST['sh']) && $_POST['sh']==$id)?1:0;
+            break;
+            case "admin":
+                $row['acc']=$_POST['acc'][$key];
+                $row['pw']=$_POST['pw'][$key];
+            break;
+            case "menu":
+                $row['href']=$_POST['href'][$key];
+                $row['sh']=(isset($_POST['sh']) && in_array($id,$_POST['sh']))?1:0;
+            break;
+            default:
+                $row['sh']=(isset($_POST['sh']) && in_array($id,$_POST['sh']))?1:0;
+        }
 
- 
- if(isset($_POST['id'])){
-     foreach($_POST['id'] as $id){
-         $_POST['text'][$id]='';
-     }
- }
- 
- foreach($_POST['text'] as $id => $text){
-     if(isset($_POST['del']) && in_array($id,$_POST['del'])){
-         $DB->del($id);
-     }else{
-         $row=$DB->find($id);
-         if(isset($row['text'])){
-             $row['text']=$text;
-         }
-         if($table=='title'){
-             $row['sh']=(isset($_POST['sh']) && $_POST['sh']==$id)?1:0;
-         }else{
-             $row['sh']=(isset($_POST['sh']) && in_array($id,$_POST['sh']))?1:0;
-         }
-         $DB->save($row);
-     }
- }
- 
- to("../back.php?do=$table");
+        $DB->save($row);
+    }
+}
+
+to("../back.php?do=$table");
